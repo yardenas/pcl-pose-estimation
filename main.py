a09787d6042324767fcac49802e875326de23ece
Yarden As <yarden.as@inf.ethz.ch>
2024-02-02 11:33:26 +0100

    Training and evaluation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import hydra
 import optax
 import jax
+import equinox as eqx
 from omegaconf import DictConfig
 
 from pcl_pose_estimation.data import make_dataset
@@ -15,7 +16,6 @@ def main(config: DictConfig) -> None:
         config.data.path, config.training.batch_size, config.data.train_split
     )
     example_x, example_y = next(iter(train_data))
-    train_data = train_data.repeat(config.training.epochs)
     assert example_x.ndim == 5
     output_dim = example_y.shape[-1]
     in_channels = example_x.shape[1]
@@ -26,9 +26,11 @@ def main(config: DictConfig) -> None:
     )
     count_params(model)
     opt = optax.adam(config.training.learning_rate)
-    model = train_model(model, opt, train_data)
-    result = evaluate(model, val_data)
-    print(f"Evaluation result: {result}")
+    opt_state = opt.init(eqx.filter(model, eqx.is_array))
+    for _ in range(config.training.epochs):
+        model, opt_state = train_model(model, opt, opt_state, train_data)
+        result = evaluate(model, val_data)
+        print(f"Evaluation result: {result}")
 
 
 if __name__ == "__main__":
--- pcl_pose_estimation/training.py
+++ pcl_pose_estimation/training.py
@@ -36,15 +36,15 @@ def update_step(
 def train_model(
     model: VoxNet,
     opt: optax.GradientTransformation,
+    opt_state: optax.OptState,
     data_generator: tfd.Dataset,
     loss_fn: Callable[[jax.Array, jax.Array], jax.Array] = optax.l2_loss,
-) -> VoxNet:
-    opt_state = opt.init(eqx.filter(model, eqx.is_array))
+) -> tuple[VoxNet, optax.OptState]:
     for step, (x, y) in enumerate(data_generator.as_numpy_iterator()):
         loss, model, opt_state = update_step(model, x, y, opt, opt_state, loss_fn)
         if step % 100 == 0:
             print(f"step {step}: loss={loss:.4f}")
-    return model
+    return model, opt_state
 
 
 def evaluate(
@@ -52,7 +52,7 @@ def evaluate(
     epoch_generator: tfd.Dataset,
     metric_fn: Callable[[jax.Array, jax.Array], jax.Array] = optax.l2_loss,
 ):
-    eval_data = [(x, y) for x, y in epoch_generator]
+    eval_data = [(x, y) for x, y in epoch_generator.as_numpy_iterator()]
     x, y = jax.tree_map(np.stack, zip(*eval_data))
     y_hat = jax.vmap(model, x)
     return metric_fn(y_hat, y).mean()
