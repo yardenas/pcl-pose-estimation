280e9c2331d9b6f6c754795d7d0e518a7d72973f
Yarden <yarden.as@inf.ethz.ch>
2024-02-02 17:29:39 +0100

    Fix bug in evaluation

--- pcl_pose_estimation/training.py
+++ pcl_pose_estimation/training.py
@@ -53,6 +53,6 @@ def evaluate(
     metric_fn: Callable[[jax.Array, jax.Array], jax.Array] = optax.l2_loss,
 ):
     eval_data = [(x, y) for x, y in epoch_generator.as_numpy_iterator()]
-    x, y = jax.tree_map(np.stack, zip(*eval_data))
+    x, y = map(np.stack, zip(*eval_data))
     y_hat = jax.vmap(model, x)
     return metric_fn(y_hat, y).mean()
